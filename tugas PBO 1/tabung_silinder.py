56bb68fa16384c6cd48ee18d8e5eb850b8389110
fatonirahman278 <148634403+fatonirahman278@users.noreply.github.com>
2024-01-04 11:50:36 +0700

    Add files via upload

---
+++ tugas PBO 1/tabung_silinder.py
@@ -0,0 +1,19 @@
+# Mengimport nilai pi
+import tkinter as tk
+from tkinter import *
+from math import pi
+# Menghitung luas permukaan dan volume tabung
+
+# Jari-jari tabung dan tinggi tabung
+jari_jari = 5
+tinggi = 10
+
+# Menghitung luas permukaan tabung
+luas_permukaan = 2 * pi * jari_jari * (jari_jari + tinggi)
+
+# Menghitung volume tabung
+volume = pi * (jari_jari ** 2) * tinggi
+
+# Menampilkan hasil
+print("Luas Permukaan Tabung: ", luas_permukaan)
+print("Volume Tabung: ", volume)
